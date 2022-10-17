minischoolX/oex

refactor: Improve LoginPrefs to handle user session efficiently
We have updated the LoginPrefs to return NonNull values instead of
Nullable for multiple methods. We have introduced a new method,
isUserLoggedIn, to verify learner's session.

Fixes: LEARNER-9066

OpenEdXMobile/src/main/java/org/edx/mobile/module/analytics/SegmentEvent.java
@@ -5,7 +5,6 @@ import android.content.res.Configuration;
 import com.segment.analytics.Properties;
 
 import org.edx.mobile.base.MainApplication;
-import org.edx.mobile.model.api.ProfileModel;
 import org.edx.mobile.module.prefs.LoginPrefs;
 
 /**
@@ -28,9 +27,9 @@ public class SegmentEvent {
         this.properties.put(Analytics.Keys.CONTEXT, cxtProps);
 
         // Set User ID in each event if a user is logged-in
-        final ProfileModel profile = new LoginPrefs(MainApplication.instance()).getCurrentUserProfile();
-        if (profile != null) {
-            this.properties.putValue(Analytics.Keys.USER_ID, profile.id);
+        final LoginPrefs loginPrefs = new LoginPrefs(MainApplication.instance());
+        if (loginPrefs.iaUserLoggedIn()) {
+            this.properties.putValue(Analytics.Keys.USER_ID, loginPrefs.getUserId());
         }
     }
 

OpenEdXMobile/src/main/java/org/edx/mobile/util/links/DefaultActionListener.java
@@ -143,7 +143,7 @@ public class DefaultActionListener implements URLInterceptorWebViewClient.Action
             return;
         }
 
-        if (environment.getLoginPrefs().getUsername() == null) {
+        if (!environment.getLoginPrefs().iaUserLoggedIn()) {
             enrollCallback.onUserNotLoggedIn(courseId, emailOptIn);
             return;
         }

OpenEdXMobile/src/main/java/org/edx/mobile/view/AuthPanelUtils.java
@@ -1,7 +1,8 @@
 package org.edx.mobile.view;
 
-import androidx.annotation.NonNull;
 import android.view.View;
+
+import androidx.annotation.NonNull;
 
 import org.edx.mobile.R;
 import org.edx.mobile.core.IEdxEnvironment;
@@ -25,7 +26,7 @@ public class AuthPanelUtils {
     }
 
     public static boolean shouldAuthPanelBeVisible(@NonNull final IEdxEnvironment environment) {
-        return null == environment.getLoginPrefs().getUsername()
+        return !environment.getLoginPrefs().iaUserLoggedIn()
                 && environment.getConfig().isNewLogistrationEnabled();
     }
 

OpenEdXMobile/src/main/java/org/edx/mobile/view/SplashActivity.java
@@ -44,7 +44,7 @@ public class SplashActivity extends ComponentActivity {
         }
 
         final IEdxEnvironment environment = MainApplication.getEnvironment(this);
-        if (environment.getUserPrefs().getProfile() != null) {
+        if (environment.getLoginPrefs().iaUserLoggedIn()) {
             environment.getRouter().showMainDashboard(SplashActivity.this);
         } else if (!environment.getConfig().isRegistrationEnabled()) {
             startActivity(environment.getRouter().getLogInIntent());

minischoolX/oexx

OpenEdXMobile/src/main/java/org/edx/mobile/module/analytics/SegmentEvent.java
@@ -5,7 +5,6 @@ import android.content.res.Configuration;
 import com.segment.analytics.Properties;
 
 import org.edx.mobile.base.MainApplication;
-import org.edx.mobile.model.api.ProfileModel;
 import org.edx.mobile.module.prefs.LoginPrefs;
 
 /**
@@ -28,9 +27,9 @@ public class SegmentEvent {
         this.properties.put(Analytics.Keys.CONTEXT, cxtProps);
 
         // Set User ID in each event if a user is logged-in
-        final ProfileModel profile = new LoginPrefs(MainApplication.instance()).getCurrentUserProfile();
-        if (profile != null) {
-            this.properties.putValue(Analytics.Keys.USER_ID, profile.id);
+        final LoginPrefs loginPrefs = new LoginPrefs(MainApplication.instance());
+        if (loginPrefs.isUserLoggedIn()) {
+            this.properties.putValue(Analytics.Keys.USER_ID, loginPrefs.getUserId());
         }
     }
 

OpenEdXMobile/src/main/java/org/edx/mobile/util/links/DefaultActionListener.java
@@ -143,7 +143,7 @@ public class DefaultActionListener implements URLInterceptorWebViewClient.Action
             return;
         }
 
-        if (environment.getLoginPrefs().getUsername() == null) {
+        if (!environment.getLoginPrefs().isUserLoggedIn()) {
             enrollCallback.onUserNotLoggedIn(courseId, emailOptIn);
             return;
         }

OpenEdXMobile/src/main/java/org/edx/mobile/view/AuthPanelUtils.java
@@ -1,7 +1,8 @@
 package org.edx.mobile.view;
 
-import androidx.annotation.NonNull;
 import android.view.View;
+
+import androidx.annotation.NonNull;
 
 import org.edx.mobile.R;
 import org.edx.mobile.core.IEdxEnvironment;
@@ -25,7 +26,7 @@ public class AuthPanelUtils {
     }
 
     public static boolean shouldAuthPanelBeVisible(@NonNull final IEdxEnvironment environment) {
-        return null == environment.getLoginPrefs().getUsername()
+        return !environment.getLoginPrefs().isUserLoggedIn()
                 && environment.getConfig().isNewLogistrationEnabled();
     }
 

OpenEdXMobile/src/main/java/org/edx/mobile/view/SplashActivity.java
@@ -44,7 +44,7 @@ public class SplashActivity extends ComponentActivity {
         }
 
         final IEdxEnvironment environment = MainApplication.getEnvironment(this);
-        if (environment.getUserPrefs().getProfile() != null) {
+        if (environment.getLoginPrefs().isUserLoggedIn()) {
             environment.getRouter().showMainDashboard(SplashActivity.this);
         } else if (!environment.getConfig().isRegistrationEnabled()) {
             startActivity(environment.getRouter().getLogInIntent());