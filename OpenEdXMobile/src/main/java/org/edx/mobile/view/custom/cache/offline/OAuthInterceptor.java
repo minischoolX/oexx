82c41b7efea815c86db73bf89e0fb0583155622e
schoddleX <115626337+schoddleX@users.noreply.github.com>
2022-10-20 00:00:50 +0530

    Create OAuthInterceptor.java

---
+++ OpenEdXMobile/src/main/java/org/edx/mobile/view/custom/cache/offline/OAuthInterceptor.java
@@ -0,0 +1,47 @@
+package org.edx.mobile.view.custom.cache.offline;
+
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+
+import org.edx.mobile.core.EdxDefaultModule;
+import org.edx.mobile.logger.Logger;
+import org.edx.mobile.module.prefs.LoginPrefs;
+
+import java.io.IOException;
+
+import dagger.hilt.android.EntryPointAccessors;
+import okhttp3.Interceptor;
+import okhttp3.Request;
+import okhttp3.Response;
+
+/**
+ * Injects OAuth token - if present - into Authorization header
+ **/
+public final class OauthInterceptor implements ResourceInterceptor {
+    protected final Logger logger = new Logger(getClass().getName());
+
+    private final LoginPrefs loginPrefs;
+
+    public OauthInterceptor(Context context) {
+        loginPrefs = EntryPointAccessors
+                .fromApplication(context, EdxDefaultModule.ProviderEntryPoint.class).getLoginPrefs();
+    }
+
+    @Override
+    @NonNull
+    public WebResource intercept(Chain chain) throws IOException {
+//        final Request.Builder builder = chain.request().newBuilder();
+        final CacheRequest request = chain.getRequest();
+        SourceRequest sourceRequest = new SourceRequest(request, true);
+        final String token = loginPrefs.getAuthorizationHeader();
+        if (token != null) {
+            Map<String, String> headers = sourceRequest.getHeaders();
+
+            headers.put("Authorization", token);
+            sourceRequest.setHeaders(headers);
+        }
+        return chain.process(request);
+    }
+
+}
